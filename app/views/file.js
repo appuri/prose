1346eec1bfb8fa4d5e2d9ce93099d5deb3774721
Nathan Black <nathan@nathanblack.org>
2016-06-29 15:47:31 -0700

    Fix live preview to handle our image layout scheme

--- app/views/file.js
+++ app/views/file.js
@@ -293,8 +293,8 @@ module.exports = Backbone.View.extend({
             path = path.split(titleAttribute)[0];
           }
 
-          // Remove {{site.baseurl}}
-          path = path.replace('{{site.baseurl}}/', '/');
+          // Rebase the image to the path in github, not where it will be displayed
+          path = path.replace('images/blog', '/site/images/blog');
 
           // Prepend directory path if not site root relative
           path = /^\//.test(path) ? path.slice(1) :
@@ -1365,7 +1365,7 @@ module.exports = Backbone.View.extend({
     this.collection.upload(file, content, uploadPath, {
       success: (function(model, res, options) {
         var name = res.content.name;
-        var path = '{{site.baseurl}}/' + res.content.path;
+        var path = res.content.path;
 
         // Take the alt text from the insert image box on the toolbar
         var $alt = $('input[name="alt"]');
